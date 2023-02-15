e231d6834d8af4f87a0c55706238db23e46f6b06
Ashritha-lopelli <124090003+Ashritha-lopelli@users.noreply.github.com>
2023-02-15 11:50:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
-api_key = "e18ee1b6e5331e253852aa61412d618e"  # Replace with your OpenWeatherMap API key
+
+zip_codes = [
+    {
+        "weather": "Cloudy",
+        "zip_code": "10001"
+    },
+    {
+        "weather": "Sunny",
+        "zip_code": "90001"
+    },
+    {
+        "weather": "Snow",
+        "zip_code": "60601"
+    }
+]
 @app.route("/weather/<city>")
 def get_weather(city):
-    # Make a request to the OpenWeatherMap API
-    zip_code = f"http://localhost:5003/zipcodes/{city}/zipcode"
-    url = f"http://api.openweathermap.org/data/2.5/forecast?zip={zip_code},us&appid={api_key}"
-    response = requests.get(url)
-
-    # Check the response status code
-    if response.status_code == 200:
-        weather_data = response.json()
-        return jsonify(weather_data)
-    else:
-        return "Error retrieving weather data", response.status_code
-
+    zip_code = requests.get(f"http://localhost:5003/zipcodes/{city}/zipcode")
+    zip_code_1 = zip_code.text
+    for zip_code in zip_codes:
+            if zip_code['zip_code'] == zip_code_1:
+                return zip_code['weather']
+    return "weather not found"
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port =5004)
